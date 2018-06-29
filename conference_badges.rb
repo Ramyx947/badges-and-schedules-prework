9855a44b51e137b5b4dc6d01b9e8d7fbf08ba217
Ramona M <r1mari@yahoo.com>
2018-06-29 11:40:57 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 speakers=["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def batch_badge_creator(speakers)
-  puts "Hello, my name is #{speaker}."
+  puts "Hello, my name is #{speaker.pop}."
 end
 
 def assign_room(speakers)
