8c2b8656e15064387382552a4b7daab0d549c15e
Ramona M <r1mari@yahoo.com>
2018-06-29 11:28:11 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,6 +4,14 @@ def batch_badge_creator(speakers)
   
 end
 
-def assign_room
-  
+def assign_room(speakers)
+ room = rand(1..7)
+ 
+ 
+ puts "Hello #{speaker}! You'll be assigned to room #{room}"
+end
+
+def printer
+  batch_badge_creator
+  assign_rooms
 end
